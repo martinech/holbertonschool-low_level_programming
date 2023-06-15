5f573c17dc16a13b28bf8147a153b53d8845fb8b
martinech <martin.echenique@hotmail.com>
2023-06-15 07:17:40 -0700

    deleted comments

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
-int main(void)
+int
+main(void)
 {
 int n;
 
