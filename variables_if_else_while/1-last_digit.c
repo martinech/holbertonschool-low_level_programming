1f4384af049266dee7f6e7b4a6b3fbedc5f9c578
martinech <martin.echenique@hotmail.com>
2023-06-15 08:24:24 -0700

    exercise 1

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int
+main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+int lastDigit = n % 10;
+if (n > 5)
+{
+	printf("Last digit of %i and is greater than 5\n", n);
+}
+if (n == 0)
+{
+	printf("Last digit of %i and is 0\n", n);
+}
+if (0 < n < 6)
+{
+	printf("Last digit of %i and is less than 6 and not 0\n", n);
+}
+return (0);
+}
