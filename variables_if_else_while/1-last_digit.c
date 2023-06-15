e6caf489b6412fd3f82cccff0fb1f9abdd8e0e7e
martinech <martin.echenique@hotmail.com>
2023-06-15 08:27:35 -0700

    swaped n for lastDigit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,15 +11,15 @@ n = rand() - RAND_MAX / 2;
 int lastDigit = n % 10;
 if (n > 5)
 {
-	printf("Last digit of %i and is greater than 5\n", n);
+	printf("Last digit of %i and is greater than 5\n", lastDigit);
 }
 if (n == 0)
 {
-	printf("Last digit of %i and is 0\n", n);
+	printf("Last digit of %i and is 0\n", lastDigit);
 }
 if (0 < n < 6)
 {
-	printf("Last digit of %i and is less than 6 and not 0\n", n);
+	printf("Last digit of %i and is less than 6 and not 0\n", lastDigit);
 }
 return (0);
 }
