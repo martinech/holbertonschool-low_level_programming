817e0f2c2d98809568cf90d9fa27b22666873ff5
martinech <martin.echenique@hotmail.com>
2023-06-15 10:25:12 -0700

    exercise 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int
+main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+	putchar(ch);
+putchar('\n');
+return (0);
+}
