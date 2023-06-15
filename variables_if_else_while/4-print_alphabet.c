0a5528f433d9835dfb8968399b1f3735f5311a64
martinech <martin.echenique@hotmail.com>
2023-06-15 10:56:45 -0700

    addede ch before !=

---
+++ variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int
+main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'q' && ch != 'e')
+{
+putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
