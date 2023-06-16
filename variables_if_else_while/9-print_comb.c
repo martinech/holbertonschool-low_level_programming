0f9b0a4ae73c51d8c1bc5f050f6c3db99ef0ad7a
martinech <martin.echenique@hotmail.com>
2023-06-16 06:24:38 -0700

    exercise 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -5,14 +5,16 @@
 int
 main(void)
 {
-int ch;
-for (ch = '0' ; ch <= '9' ; ch++)
+int ch1;
+int ch2;
+for (ch1 = '0' ; ch1 <= '9' ; ch1++)
 {
-	putchar(ch);
-}
-for (ch = 'a' ; ch <= 'f' ; ch++)
-{
-	putchar(ch);
+	for (ch2 = '0' ; ch2 <= '9' ; ch2++)
+	{
+	putchar (ch1);
+	putchar (", ");
+	putchar (ch2);
+	}
 }
 putchar('\n');
 return (0);
