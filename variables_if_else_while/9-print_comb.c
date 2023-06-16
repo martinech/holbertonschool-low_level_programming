e9baed771dcd451674e2206d78df8e5d51917834
martinech <martin.echenique@hotmail.com>
2023-06-16 06:40:00 -0700

    fixed script of ex 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -5,16 +5,12 @@
 int
 main(void)
 {
-int ch1;
-int ch2;
-for (ch1 = '0' ; ch1 <= '9' ; ch1++)
+char ch;
+for (ch = '0' ; ch <= '9' ; ch++)
 {
-	for (ch2 = '0' ; ch2 <= '9' ; ch2++)
-	{
-	putchar (ch1);
-	putchar (", ");
-	putchar (ch2);
-	}
+putchar(ch);
+putchar(',');
+putchar(' ');
 }
 putchar('\n');
 return (0);
