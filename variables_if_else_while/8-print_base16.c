4437758b68c03f91b835fb78c58b432a3e8c66ef
martinech <martin.echenique@hotmail.com>
2023-06-16 05:56:15 -0700

    exercise 8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -6,12 +6,14 @@ int
 main(void)
 {
 char ch;
-if (ch <= '10')
-	{
-	for (ch = '0' ; ch <= '16' ; ch++)
-	{
-		putchar(ch);
-	
+for (ch = '0' ; ch <= '9' ; ch++)
+{
+	putchar(ch);
+}
+for (ch = 'a' ; ch <= 'f' ; ch++)
+{
+	putchar(ch);
+}
 putchar('\n');
 return (0);
 }
