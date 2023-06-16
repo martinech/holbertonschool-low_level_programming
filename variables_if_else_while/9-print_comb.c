a8be4ed7918d18d225109ce16de14c488ecf5fe8
martinech <martin.echenique@hotmail.com>
2023-06-16 07:48:15 -0700

    added an if

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,9 +8,12 @@ main(void)
 int ch;
 for (ch = '0' ; ch <= '9' ; ch++)
 {
-putchar(ch);
-putchar(',');
-putchar(' ');
+	putchar(ch);
+	if (ch != 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
 }
 putchar('\n');
 return (0);
