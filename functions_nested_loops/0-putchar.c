8a99b42358265be96ddced550a2262e887563b87
martinech <martin.echenique@hotmail.com>
2023-06-16 12:18:49 -0700

    added =

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,12 +5,10 @@
 int
 main(void)
 {
-char frase[]"_putchar";
+char frase[] = "_putchar";
 int i;
 for (i = 0; i <= 7; i++)
-{
-	_putchar(frase[i]);
-	_putchar('\n');
-}
+_putchar(frase[i]);
+_putchar('\n');
 return (0);	
 }
