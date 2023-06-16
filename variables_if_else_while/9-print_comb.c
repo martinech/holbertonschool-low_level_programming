a6b9ea1ce205c591c11c24bd7d5bc7d334d989aa
martinech <martin.echenique@hotmail.com>
2023-06-16 08:12:13 -0700

    used while

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -6,14 +6,11 @@ int
 main(void)
 {
 int ch;
-for (ch = '0' ; ch <= '9' ; ch++)
+while (ch < '9')
 {
-	putchar(ch);
-	if (ch != 9)
-	{
+putchar(ch);
 	putchar(',');
 	putchar(' ');
-	}
 }
 putchar('\n');
 return (0);
