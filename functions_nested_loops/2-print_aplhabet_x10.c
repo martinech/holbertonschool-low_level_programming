8f22c376695cb29939dff454cba47828c08e5e30
martinech <martin.echenique@hotmail.com>
2023-06-18 09:04:29 -0700

    exercise 2

---
+++ functions_nested_loops/2-print_aplhabet_x10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+void
+print_alphabet(void)
+{
+char i;
+int x;
+for (x = 0; x <= 10; x++)
+	for (i = 'a'; i <= 'z'; i++)
+	_putchar(i);
+	_putchar('\n');
+}
