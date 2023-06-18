8595e87e3a0efc538f9d6a2c731a0ff8607dec09
martinech <martin.echenique@hotmail.com>
2023-06-18 07:53:04 -0700

    exercise 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,12 +2,13 @@
 
 void
 print_alphabet(void);
+
 {
-char i;
-for (i = 'a'; i <= 'z'; i++)
-{
-_putchar(i);
-}
-_putchar('\n');
-return (0);
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
