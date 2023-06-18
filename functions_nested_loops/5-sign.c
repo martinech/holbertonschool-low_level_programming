5408e52a638723fc2b9c1aa449929f8e8d21d00f
martinech <martin.echenique@hotmail.com>
2023-06-18 16:52:45 -0700

    changed order of conditions

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,14 +10,14 @@ print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n = 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
 	}
 }
