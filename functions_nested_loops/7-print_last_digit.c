e53d383c0f53d8845d1099c7c2bfb8800eba8728
martinech <martin.echenique@hotmail.com>
2023-06-18 17:31:30 -0700

    exercise 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+int
+print_last_digit(int n)
+
+{
+	int p;
+
+	if (n < 0)
+	{
+		p = (-1 * (n % 10));
+			_putchar(p + '0');
+	}
+	else
+	{
+		p = (n % 10);
+			_putchar(p + '0');
+	}
+	return (p)
+}
