e4a134e3cdc481083258bda2b52ab3f60bd3a928
martinech <martin.echenique@hotmail.com>
2023-06-18 19:49:07 -0700

    exercise 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+nclude <stdio.h>
+#include "main.h"
+
+void
+print_to98(int x)
+{
+	if (x < 98)
+	{
+		while (x < 98)
+		{
+			printf("%d, ", x);
+			x++;
+		}
+	}
+	else if (x > 98)
+	{
+		while (x > 98)
+		{
+			printf("%d, ", x);
+			x--;
+		}
+	}
+	printf("98\n");
+}
