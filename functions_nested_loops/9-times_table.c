9df57ad28031d4784b26fb445279811d3b018e92
martinech <martin.echenique@hotmail.com>
2023-06-18 19:30:14 -0700

    exercise 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+void
+times_table(void)
+{
+	int x, y, first, second;
+	int counter = '0';
+
+	for (counter = 0; counter <= 9; counter++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			y = x * counter;
+			first = (y / 10 + '0');
+			second = (y % 10 + '0');
+
+			if (y >= 10)
+			{
+				_putchar(first);
+				_putchar(second);
+			}
+			else if (x != 0)
+			{
+				_putchar(' ');
+				_putchar(y + '0');
+			}
+			else
+			{
+				_putchar(y + '0');
+			}
+			
+			if (x != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
