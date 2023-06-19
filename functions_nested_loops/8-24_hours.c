668c2fb059dd1d5212f4946a0294841d8892d8b1
martinech <martin.echenique@hotmail.com>
2023-06-18 19:12:49 -0700

    exercise 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+void
+jack_bauer(void)
+
+{
+	int p;
+	int r;
+
+	for (p = 0; h < 24; h++)
+	{
+		for (r = 0; r <= 59; r++)
+		{
+			_putchar((p / 10) + '0');
+			_putchar((p % 10) + '0');
+			_putchar(':');
+			_putchar((r / 10) + '0');
+			_putchar((r % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
