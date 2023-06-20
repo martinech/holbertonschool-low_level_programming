403dc9b9885330a9799d004a1e3ac623432da051
martinech <martin.echenique@hotmail.com>
2023-06-20 14:17:38 -0700

    fixed betty

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -5,7 +5,6 @@ int
 main(void)
 {
 	int i;
-
 	for (i = 1; i <= 100; i++)
 	{
 			if (i % 3 == 0 && i % 5 == 0)
@@ -21,6 +20,5 @@ main(void)
 					printf("%d ", i);
 	}
 	printf("\n");
-
 	return (0);
 }
