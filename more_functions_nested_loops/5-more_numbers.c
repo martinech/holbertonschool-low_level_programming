d894b658836f25ade587ff0c5644cc85a61236a4
martinech <martin.echenique@hotmail.com>
2023-06-20 10:19:18 -0700

    exercise 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+void
+more_numbers(void)
+{
+	char i;
+	int x;
+	
+	for (x = 0 ; i <= 10; x++)
+		for (i = '0'; i <= '14'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+}
