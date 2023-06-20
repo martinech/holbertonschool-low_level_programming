660f67a00757584e1cd2ebeffbe519417df072c5
martinech <martin.echenique@hotmail.com>
2023-06-20 08:11:02 -0700

    added ; to int c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,18 @@
 #include <string.h>
 #include "main.h"
 
+/**
+ * @c: variable
+ */
+
 void
 print_numbers(void)
 
 {
 
-int c
+int c;
 
-	for (c = 0, c <= 9, c++)
+	for (c = 0; c <= 9; c++)
 	{
 		_putchar('c');
 		_putchar('\n');
