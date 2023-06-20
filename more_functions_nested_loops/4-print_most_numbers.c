9736d71f18a0e9640ca28846741c0c7b5ce3ffb1
martinech <martin.echenique@hotmail.com>
2023-06-20 09:46:59 -0700

    exercise 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int
+main(void)
+{
+int i;
+for (i = '0'; ch <= '9'; i++)
+{
+if (i != '2' && ch != '4')
+{
+putchar(i);
+}
+}
+putchar('\n');
+return (0);
+}
