1764cd80ee70ffecc21fc522ac6e2f79f49e1d18
martinech <martin.echenique@hotmail.com>
2023-06-20 14:06:50 -0700

    fixed betty

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -3,12 +3,12 @@
 void
 print_line(int n)
 {
-		int line = 0;
+	int line = 0;
 
-			while (line < n)
-					{
-								_putchar('_');
-										line++;
-											}
-				_putchar('\n');
+	while (line < n)
+	{
+		_putchar('_');
+		line++;
+	}
+	_putchar('\n');
 }
