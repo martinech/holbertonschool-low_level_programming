87871bc9506611fa0549630a05508bafe9c8743c
martinech <martin.echenique@hotmail.com>
2023-06-20 10:41:38 -0700

    fixed betty

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,17 +5,17 @@
 void
 more_numbers(void)
 {
-	int i,x;
-	
+	int i, x;
+
 	for (x = 0 ; x <= 9; x++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
 			if (i >= 10)
 			{
-				_putchar((i/10) + '0');
+				_putchar((i / 10) + '0');
 			}
-			_putchar((i%10) + '0');
+			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
 	}
