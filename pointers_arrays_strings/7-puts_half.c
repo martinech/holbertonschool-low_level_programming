c62e91333098cfb73ca6ade6f238c4c9796c34bb
martinech <martin.echenique@hotmail.com>
2023-06-23 13:19:28 -0700

    exercise 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+void
+puts_half(char *str)
+{
+int n;
+int c[1000];
+
+while (*str)
+{
+	len++;
+}
+n = (len - 1) / 2
+
+while (n < len)
+{
+	c[n]=str[n];
+}	
