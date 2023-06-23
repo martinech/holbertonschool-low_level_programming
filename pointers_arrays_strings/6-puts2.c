aa670f4facd011baad2217cfe1408d08d47ace30
martinech <martin.echenique@hotmail.com>
2023-06-23 11:27:03 -0700

    exercise 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+void
+puts2(char *s)
+{
+	
+int len = 0;
+
+while (s[len] != '\0')
+	len++;
+while (len > 0)
+	{
+	len--;
+	_putchar(s[2*len]);
+}
+
+_putchar('\n');
+}
