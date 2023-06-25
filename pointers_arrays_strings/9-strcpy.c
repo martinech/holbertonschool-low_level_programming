f101b965816b50db78ca6352cb9e5918aff46748
martinech <martin.echenique@hotmail.com>
2023-06-25 15:18:16 -0700

    exercise 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array
+ * @a: array of integers
+ * @n: number of items to be printed
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+	
+		if (i == n - 1)
+			continue;
+
+	
+		printf(", ");
+	}
+	printf("\n");
+}
