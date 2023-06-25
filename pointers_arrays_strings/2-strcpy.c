e407d760d0481ce9fd42c9790b414d8c769c4af7
martinech <martin.echenique@hotmail.com>
2023-06-25 16:27:34 -0700

    exercise 2

---
+++ pointers_arrays_strings/2-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+char
+*_strncpy(char *dest, char *src, int n)
+{
+	int dLen = 0;
+
+
+	while (dLen < n && src[dLen] != '\0')
+	{
+		dest[dLen] = src[dLen];
+		dLen++;
+	}
+
+
+	while (dLen < n)
+	{
+		dest[dLen] = '\0';
+		dLen++;
+	}
+
+
+	return (dest);
+}
