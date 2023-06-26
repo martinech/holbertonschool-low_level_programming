086fa5dd75c92011763561f7418a4d771a59896f
martinech <martin.echenique@hotmail.com>
2023-06-26 05:33:51 -0700

    exercise 4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void
+reverse_array(int *a, int n);
+{
+
+int l = 0;
+int na[];
+int count;
+
+while (*a != '\0')
+	l++;
+for (count, count <= l, count++)
+{
+	na[count]=a[l];
+	l--;
+}
+}
+
