a9a88359284d987b20a448f4a243c0d25e3f72d8
martinech <martin.echenique@hotmail.com>
2023-06-26 11:01:06 -0700

    fixede typos

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 char
-*string_toupper(char *)
+*string_toupper(char *s)
 {
 int l;
 
-for (l = 0; s[l] != '\0', l++)
+for (l = 0; s[l] != '\0'; l++)
 	{
 	if (97 <= s[l] <= 122)
 		 s[l] = s[l] + 32;
