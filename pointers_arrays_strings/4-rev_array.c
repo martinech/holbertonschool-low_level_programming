bf849d4ebf6537e458040e97c552cb4638a94317
martinech <martin.echenique@hotmail.com>
2023-06-26 06:41:14 -0700

    fixed for

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -5,12 +5,12 @@ reverse_array(int *a, int n)
 {
 
 int l = 0;
-int na[1];
-int count;
+int na[n];
+int count = 0;
 
 while (*a != '\0')
 	l++;
-for (count = 0; count <= l; count++);
+for (; count <= l; count++);
 {
 	na[count]=a[l];
 	l--;
