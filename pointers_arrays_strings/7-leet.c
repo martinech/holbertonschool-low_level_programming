3f856737a487cf881b5ca8c59ca3ab3a41b06c19
martinech <martin.echenique@hotmail.com>
2023-06-26 18:54:29 -0700

    exercise 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+char 
+*leet(char *str)
+{
+	int i, j;
+	char strL[] = "AaEeOoTtLl";
+	char strN[] = "4433007711";
+
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == strL[j])
+			{
+				str[i] = strN[j];
+			}
+		}
+	}
+	return (str);
+}
