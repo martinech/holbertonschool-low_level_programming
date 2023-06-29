1729cdd67ef5d43c52dc623f3cb1f4d6784e8ff4
martinech <martin.echenique@hotmail.com>
2023-06-29 13:01:24 -0700

    exercise 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,10 +1,18 @@
 #include "main.h"
 
-int
-is_prime_number(int n)
+int is_prime_number(int n)
 {
-	if (n % is_prime_number(n) == 0)
-		return (1);
-	else
+	if (n <= 1)
 		return (0);
+	return (_prime(n, n - 1));
+}
+
+
+int _prime(int n, int m)
+{
+	if (n % m == 0 && m != 1)
+		return (0);
+	if (n % m == 0 && m == 1)
+		return (1);
+	return (_prime(n, m - 1));
 }
