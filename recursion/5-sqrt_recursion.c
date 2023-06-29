d4c716655d405f77f7c02589a80fd73000886690
martinech <martin.echenique@hotmail.com>
2023-06-29 12:40:34 -0700

    exercise 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+int
+aux(int a, int b)
+{
+	if ((b * b) == a)
+		return (b);
+
+
+	if (b == a / 2)
+		return (-1);
+
+
+	return (aux(a, b + 1));
+}
+
+int
+_sqrt_recursion(int n)
+{
+	int b = 0;
+
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+
+	if (n == 1)
+	{
+		return (1);
+	}
+
+
+	return (aux(n, b));
+}
